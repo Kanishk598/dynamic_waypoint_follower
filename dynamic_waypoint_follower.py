0a098c6f4a462e944099a8ab541b0cc966eacc4e
Kanishk Vishwakarma <kanishk.vishwa2001@gmail.com>
2021-08-21 15:33:40 +0530

    Final working code

--- dynamic_waypoint_follower.py
+++ dynamic_waypoint_follower.py
@@ -3,6 +3,7 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Odometry
+import math
 
 rospy.init_node("waypoint")
 
@@ -11,27 +12,26 @@ class follower:
         self.goal = PoseStamped()
         self.goals = []
         self.odom = Odometry()
+        self.dist = 0
         self.latch = 0
         self.sub_goal = rospy.Subscriber("/new_goal", PoseStamped, self.callback_goal)
         self.sub_odom = rospy.Subscriber("/odom", Odometry, self.callback_odom)
         self.pub_goal = rospy.Publisher("/move_base_simple/goal", PoseStamped, queue_size = 1)
     def callback_goal(self, message):
         self.goal = message
+        print("Got a new goal")
         self.goals.append(self.goal)
     def callback_odom(self, message):
         self.odom = message
-        print("Number of goals in the list: " + str(len(self.goals)))
         if (len(self.goals) != 0):
-            dist = ((self.goals[0].pose.position.x - self.odom.pose.pose.position.x)**2 + (self.goals[0].pose.position.y - self.odom.pose.pose.position.y)**2)**(1/2)
-            if (dist > 0.5 and self.latch == 0):
+            self.dist = math.sqrt((self.goals[0].pose.position.x - self.odom.pose.pose.position.x)**2 + (self.goals[0].pose.position.y - self.odom.pose.pose.position.y)**2)
+            if (self.dist > 0.1 and self.latch == 0):
                 self.pub_goal.publish(self.goals[0])
                 self.latch = 1
-                print("Published a goal")
-            elif (self.latch==1):
-                if(dist <=0.5):
+            else:
+                if self.dist <= 0.5:
                     del(self.goals[0])
                     self.latch = 0
-                    print("Deleted a goal")
 
 follower = follower()
 rospy.spin()
